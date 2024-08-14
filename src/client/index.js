9eb30928879011a34eb1f2deed279a17653d8771
MKOdeh2024 <217103@ppu.edu.ps>
2024-08-14 13:30:26 +0300

    final touches

--- src/client/index.js
+++ src/client/index.js
@@ -10,14 +10,19 @@ import './styles/header.scss'
 console.log(checkForName);
 
 // Define the onBlur function
-function onBlur() {
+/**function onBlur() {
     console.log('Input field lost focus');
     // Add any additional logic you want to execute when the input loses focus
-}
+}**/
 
 
 // Make functions available globally
-window.onblur = onBlur;
+//window.onblur = onBlur;
 // Rest of your JavaScript code
 alert("I EXIST")
 console.log("CHANGE!!");
+
+export {
+    checkForName,
+    handleSubmit
+}
